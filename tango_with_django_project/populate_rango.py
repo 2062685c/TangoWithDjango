d6fd9ec5c34d715d58bd039a8da46e2cfb74ac4a
unknown <2062685c@BO706-35.ad.dcs.gla.ac.uk>
2015-01-26 12:31:08 +0000

    Chapter 7 - completed

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -54,7 +54,7 @@ def populate():
 
     add_page(cat=me_cat,
         title="PythonAnywhere",
-        url="https://www.pythonanywhere.com/user/2062685c/consoles/")
+        url="https://www.pythonanywhere.com/user/2062685c/consoles/", views=50)
 
     # Print out what we have added to the user.
     for c in Category.objects.all():
@@ -65,7 +65,7 @@ def add_page(cat, title, url, views=0):
     p = Page.objects.get_or_create(category=cat, title=title, url=url, views=views)[0]
     return p
 
-def add_cat(name, views, likes):
+def add_cat(name, views=0, likes=0):
     c = Category.objects.get_or_create(name=name, views=views, likes=likes)[0]
     return c
 
